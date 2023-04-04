abeacdb8b2e0bfd6b2c1b13cb00127b140b80a3c
NavBernal <navjit.bernal@gmail.com>
2023-04-04 00:48:26 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Navjit on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
